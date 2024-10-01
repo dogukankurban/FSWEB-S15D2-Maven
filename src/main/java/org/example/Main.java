04d8d8b2913e38f1f85af55286fc359c5fc8f7db
dogukankurban <c.d.kurban007@gmail.com>
2024-10-01 20:51:56 +0300

    s15d2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import org.example.entity.StringSet;
+import org.example.entity.TaskData;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        StringSet.findUniqueWords();
+
     }
 }
